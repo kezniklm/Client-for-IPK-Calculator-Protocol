548a7a8d1333fa0bd63c5f3a5c27f58ae5b8ae54
Matej Keznikl <matej.keznikl@gmail.com>
2023-03-16 10:42:22 +0100

    Started working on UDP communication

---
+++ udp.c
@@ -0,0 +1,6 @@
+/**
+ * @file udp.c
+ * @author Matej Keznikl (xkezni01@stud.fit.vutbr.cz)
+ * @brief Funkcie pre UDP komunikáciu klienta pre IPK Calculator Protokol
+ * @date 2023-03-21
+ */
---
+++ udp.h
@@ -0,0 +1,6 @@
+/**
+ * @file udp.h
+ * @author Matej Keznikl (xkezni01@stud.fit.vutbr.cz)
+ * @brief Hlavičkové súbory pre UDP komunikáciu klienta pre IPK Calculator Protokol
+ * @date 2023-03-21
+ */
